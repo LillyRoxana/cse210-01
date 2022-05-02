e360ed40203e0695b2fd097bd8b09e1485abb338
LillyRoxana <78121782+LillyRoxana@users.noreply.github.com>
2022-05-02 17:19:45 -0500

    Update tic-tac-poe.py

--- tic-tac-poe.py
+++ tic-tac-poe.py
@@ -1,46 +1,50 @@
-'''
-
 # REQUIREMENTS:
-
 # The program must have at least one if/then block.
 # The program must have at least one while loop.
 # The program must have more than one function.
 # The program must have a function called main.
 
-'''
-
 def main():
     player = next_player("")
-    board = create_board()
-    while not (has_winner(board) or is_a_draw(board)):
+    board = new_board()
+    while not (winner(board) or draw(board)):
         display_board(board)
-        make_move(player, board)
+        next_move(player, board)
         player = next_player(player)
     display_board(board)
-    print("Good game. Thanks for playing!") 
+    print("Thanks for playing!") 
 
-def create_board():
+def new_board():
     board = []
-    for square in range(9):
-        board.append(square + 1)
+    for value in range(9):
+        board.append(value + 1)
     return board
 
-def display_board(board):
-    print()
-    print(f"{board[0]}|{board[1]}|{board[2]}")
-    print('-+-+-')
-    print(f"{board[3]}|{board[4]}|{board[5]}")
-    print('-+-+-')
-    print(f"{board[6]}|{board[7]}|{board[8]}")
-    print()
-    
-def is_a_draw(board):
-    for square in range(9):
-        if board[square] != "x" and board[square] != "o":
+def draw(board):
+    for value in range(9):
+        if board[value] != "x" and board[value] != "o":
             return False
     return True 
+
+def display_board(board):
+    print("     |     |")
+    print("  {}  |  {}  |  {}".format(board[0],board[1], board[2]))
+    print("_____|_____|_____")
+    print("     |     |")
+    print("  {}  |  {}  |  {}".format(board[3],board[4], board[5]))
+    print("_____|_____|_____")
+    print("     |     |")
+    print("  {}  |  {}  |  {}".format(board[6],board[7], board[8]))
+    print("     |     |")
+    print("\n")
+
+
+def next_move(player, board):
+    value = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[value - 1] = player
     
-def has_winner(board):
+    
+def winner(board):
     return (board[0] == board[1] == board[2] or
             board[3] == board[4] == board[5] or
             board[6] == board[7] == board[8] or
@@ -50,15 +54,13 @@ def has_winner(board):
             board[0] == board[4] == board[8] or
             board[2] == board[4] == board[6])
 
-def make_move(player, board):
-    square = int(input(f"{player}'s turn to choose a square (1-9): "))
-    board[square - 1] = player
-
-def next_player(current):
-    if current == "" or current == "o":
+def next_player(now):
+    if now == "" or now == "o":
         return "x"
-    elif current == "x":
+    elif now == "x":
         return "o"
 
 if __name__ == "__main__":
     main()
+
+    
